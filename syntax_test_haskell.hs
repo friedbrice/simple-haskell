a2cdeb2e870050eaf72a69a7b2e7ce66ee984b19
Daniel Brice <danielbrice@gmail.com>
2018-09-24 22:29:58 -0700

    copied over all the existing tests. next step is to make them pass

--- syntax_test_haskell.hs
+++ syntax_test_haskell.hs
@@ -1,27 +1,177 @@
--- SYNTAX TEST "Packages/Haskell/Haskell.sublime-syntax"
-{-# LANGUAGE FlexibleInstances, OverloadedStrings #-}
---  ^^^^^^^^ meta.preprocessor.haskell keyword.other.preprocessor
+-- SYNTAX TEST "Packages/User/Haskell.sublime-syntax"
+{-# LANGUAGE FlexibleInstances, OverloadedStrings, UnicodeSyntax #-}
+--  ^^^^^^^^ keyword.other.preprocessor.haskell
 
 module Main (
--- ^^^ keyword.other
     Foo(str, int),
---  ^^^ meta.declaration.exports.haskell storage.type
     -- a nice comment in the module exports list
     Fooable(..),
     DeriveMoreThanOne,
---  ^^^^^^^^^^^^^^^^^ meta.declaration.exports.haskell storage.type
     main,
-    add,
+    addEmUp,
     (-->),
---   ^^^ meta.declaration.exports.haskell keyword.operator
     (+:),
---   ^^^ meta.declaration.exports.haskell keyword.operator
 ) where
--- ^^^^ keyword.other
+
+import Prelude hiding (Traversable(traverse, sequenceA))
+
 import System.Exit (ExitCode(..), exitWith)
---                  ^^^^^^^^ storage.type
 import Data.String
--- ^^^ keyword.other
+
+----
+-- Original Tests
+----
+
+x0 = 23*36 -- single line comment
+--         ^^ punctuation.definition.comment.haskell
+--         ^^^^^^^^^^^^^^^^^^^^^^^ comment.line.double-dash.haskell
+x1 = 23*36
+-- <- - comment.line.double-dash.haskell
+
+x2 = {- block comment -} 23*36
+--   ^^ punctuation.definition.comment.begin.haskell
+--   ^^^^^^^^^^^^^^^^^^^ comment.block.haskell
+--                    ^^ punctuation.definition.comment.end.haskell
+--                      ^ - comment.block.haskell
+
+x3 = {- {-# #-} -} 23*36
+--   ^^ punctuation.definition.comment.begin.haskell
+--   ^^^^^^^^^^^^^ comment.block.haskell - meta.preprocessor.haskell
+--              ^^ punctuation.definition.comment.end.haskell
+--                ^ - comment.block.haskell
+
+x4 = {- {- #-} -} 23*36
+--   ^^ punctuation.definition.comment.begin.haskell
+--   ^^^^^^^^^^^^ comment.block.haskell
+--             ^^ punctuation.definition.comment.end.haskell
+--               ^ - comment.block.haskell
+
+x5 = {- {- -} -} 23*36
+--   ^^ punctuation.definition.comment.begin.haskell
+--   ^^^^^^^^^^^ comment.block.haskell
+--            ^^ punctuation.definition.comment.end.haskell
+--              ^ - comment.block.haskell
+
+x6 = {- {-# -} -} 23*36
+--   ^^ punctuation.definition.comment.begin.haskell
+--   ^^^^^^^^^^^^ comment.block.haskell - meta.preprocessor.haskell
+--             ^^ punctuation.definition.comment.end.haskell
+--               ^ - comment.block.haskell
+
+x7 = {- {-# {- test -} -} -} 23*36
+--   ^^ punctuation.definition.comment.begin.haskell
+--   ^^^^^^^^^^^^^^^^^^^^^^^ comment.block.haskell - meta.preprocessor.haskell
+--                        ^^ punctuation.definition.comment.end.haskell
+--                          ^ - comment.block.haskell
+
+class (Functor t, Foldable t) => Traversable t where
+-- ^^ keyword.other.haskell
+-- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ meta.declaration.class.haskell
+    {-# MINIMAL traverse | sequenceA #-}
+--  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ meta.preprocessor.haskell
+--                                      ^ - meta.preprocessor.haskell
+
+    -- | Map each element of a structure to an action,
+    -- evaluate these actions from left to right, and
+    -- collect the results. For a version that ignores
+    -- the results see 'Data.Foldable.traverse_'.
+--  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ comment.line.double-dash.haskell
+    traverse :: Applicative f =>
+--  ^^^^^^^^ entity.name.function.haskell
+--           ^^ keyword.other.double-colon.haskell
+--              ^^^^^^^^^^^ storage.type.haskell
+--  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ meta.function.type-declaration.haskell
+--                            ^^ keyword.other.big-arrow.haskell
+        (a -> f b)
+--     ^^^^^^^^^^^^ meta.function.type-declaration.haskell
+--         ^^ keyword.other.arrow.haskell
+        -> t a
+--     ^^^^^^^^ meta.function.type-declaration.haskell
+--      ^^ keyword.other.arrow.haskell
+        -> f (t b)
+--     ^^^^^^^^^^^^ meta.function.type-declaration.haskell
+--      ^^ keyword.other.arrow.haskell
+    traverse f = sequenceA . fmap f
+--  ^^^^^^^^^^^^^ meta.function.type-declaration.haskell
+--             ^ keyword.operator.haskell
+--                         ^ keyword.operator.haskell
+
+    -- | Evaluate each action in the structure from
+    -- left to right, and collect the results.
+    -- For a version that ignores the results see
+    -- 'Data.Foldable.sequenceA_'.
+    sequenceA ∷ Applicative f ⇒ t (f a) → f (t a)
+--  ^^^^^^^^^ entity.name.function.haskell
+--            ^ keyword.other.double-colon.haskell
+--              ^^^^^^^^^^^ storage.type.haskell
+--                            ^ keyword.other.big-arrow.haskell
+--                                       ^ keyword.other.arrow.haskell
+    sequenceA = traverse id
+--            ^ keyword.operator.haskell
+
+----
+-- Specific Tests
+----
+
+-- deriving more than one type class
+data DeriveMoreThanOne = MkDeriveMoreThanOne deriving (Eq, Read, Show)
+
+-- arrows
+toUnit :: DeriveMoreThanOne -> ()
+toUnit _ = ()
+
+-- question marks in operators
+(!!?) :: DeriveMoreThanOne -> DeriveMoreThanOne -> DeriveMoreThanOne
+_ !!? _ = MkDeriveMoreThanOne
+
+-- operators that start with '--'
+(-->) :: (a -> b) -> (b -> c) -> a -> c
+f --> g = g . f
+
+-- operators that end with '--'
+(<--) :: (b -> c) -> (a -> b) -> a -> c
+f <-- g = f . g
+
+-- comments in class and instance declarations
+class SomeClass a -- some comment
+instance SomeClass a -- some comment
+
+-- classes can have numbers and primes in their names
+class Class0 a
+instance Class0 Int
+
+class Class' a
+instance Class' Int
+
+-- class constraints and contexts
+class (Class0 a, Class' a) => Class0' a
+instance Class0 a => Class0 (Maybe a)
+instance Class' a => Class' (Maybe a)
+instance (Class0 a, Class' a) => Class0' (Maybe a)
+
+-- functions with primes in their names
+someFunc' :: Char -> Int -> String
+someFunc' char int = replicate int char
+
+-- types with primes in their names
+newtype Foo' = Foo' { unFoo' :: Foo }
+
+-- operators, guards, and contexts
+(+:) :: Enum a => Int -> a -> a
+n +: x
+    | n <= 0    = x
+    | otherwise = (n - 1) +: succ x
+
+-- infix functions, operator sections, list literals, char literals
+someList :: [Char]
+someList = (5 +:) `fmap1'` ['a', 'b', 'c']
+  where
+    fmap1' :: (Char -> Char) -> [Char] -> [Char]
+    fmap1' = fmap
+
+----
+-- Random Stuff
+----
 
 main :: IO ()
 main = do
@@ -29,13 +179,13 @@ main = do
     let x = 1 + 1
     let y = (+) 2 2
     z <- pure 3
-    print (add x y z)
+    print (addEmUp x y z)
     putStrLn (str myFoo)
     exitWith ExitSuccess
 
-add
+addEmUp
     :: Int -> Int -> Int -> Int
-add x y z = x + y + z
+addEmUp x y z = x + y + z
 
 data Foo = Foo
     { str :: String
@@ -97,63 +247,3 @@ multiLineString =
 data OneLine a = OneLine { unOneLine :: a }
 
 class OneLiner a where oneLiner :: a -> OneLine a
-
-----
--- Check For Regressions
-----
-
--- deriving more than one type class
-data DeriveMoreThanOne = MkDeriveMoreThanOne deriving (Eq, Read, Show)
-
--- arrows
-toUnit :: DeriveMoreThanOne -> ()
-toUnit _ = ()
-
--- question marks in operators
-(!!?) :: DeriveMoreThanOne -> DeriveMoreThanOne -> DeriveMoreThanOne
-_ !!? _ = MkDeriveMoreThanOne
-
--- operators that start with '--'
-(-->) :: (a -> b) -> (b -> c) -> a -> c
-f --> g = g . f
-
--- operators that end with '--'
-(<--) :: (b -> c) -> (a -> b) -> a -> c
-f <-- g = f . g
-
--- comments in class and instance declarations
-class SomeClass a -- some comment
-instance SomeClass a -- some comment
-
--- classes can have numbers and primes in their names
-class Class0 a
-instance Class0 Int
-
-class Class' a
-instance Class' Int
-
--- class constraints and contexts
-class (Class0 a, Class' a) => Class0' a
-instance Class0 a => Class0 (Maybe a)
-instance Class' a => Class' (Maybe a)
-instance (Class0 a, Class' a) => Class0' (Maybe a)
-
--- functions with primes in their names
-someFunc' :: Char -> Int -> String
-someFunc' char int = replicate int char
-
--- types with primes in their names
-newtype Foo' = Foo' { unFoo' :: Foo }
-
--- operators, guards, and contexts
-(+:) :: Enum a => Int -> a -> a
-n +: x
-    | n <= 0    = x
-    | otherwise = (n - 1) +: succ x
-
--- infix functions, operator sections, list literals, char literals
-someList :: [Char]
-someList = (5 +:) `fmap1'` ['a', 'b', 'c']
-  where
-    fmap1' :: (Char -> Char) -> [Char] -> [Char]
-    fmap1' = fmap
